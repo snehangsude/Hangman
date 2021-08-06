e73334798ac9b7d49aa274952877d858ff4f3ff2
Snehangsu De <63997773+snehangsude@users.noreply.github.com>
2021-08-07 02:42:39 +0530

    Bug fixes

--- data.py
+++ data.py
@@ -1,5 +1,9 @@
-import requests
-
+try:
+  import requests
+except ModuleNotFoundError:
+    print("[-] Requirements not satisfied!\n\trun 'pip install -r requirements.txt'\n\t\tOR\n\trun 'pip3 install -r requirements.txt'")
+    quit()
+  
 stages = ['''
   +----+
   |    
